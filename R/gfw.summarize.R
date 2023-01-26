d6737e7454f2e1cdc4199c00d54b5bed490cac97
Shyentist <pasqualebuonomo@hotmail.it>
2023-01-26 10:32:37 +0100

    fixing cran issues with assign() function

--- R/gfw.summarize.R
+++ R/gfw.summarize.R
@@ -26,20 +26,20 @@
 gfw.summarize <- function(df){
 
   summarized <- summarize(df,
-                        "Total fishing" = sum(fishing_hours),
-                        "Min. fishing" = min(fishing_hours),
-                        "1st Qu. fishing" = quantile(fishing_hours, 0.25),
-                        "Median fishing" = median(fishing_hours),
-                        "Mean fishing" = mean(fishing_hours),
-                        "3rd Qu. fishing" = quantile(fishing_hours, 0.75),
-                        "Max. fishing" = max(fishing_hours),
-                        "Total hours" = sum(hours),
-                        "Min. hours" = min(hours),
-                        "1st Qu. hours" = quantile(hours, 0.25),
-                        "Median hours" = median(hours),
-                        "Mean hours" = mean(hours),
-                        "3rd Qu. hours" = quantile(hours, 0.75),
-                        "Max. hours" = max(hours)
+                        "Total fishing" = sum(.data$fishing_hours),
+                        "Min. fishing" = min(.data$fishing_hours),
+                        "1st Qu. fishing" = quantile(.data$fishing_hours, 0.25),
+                        "Median fishing" = median(.data$fishing_hours),
+                        "Mean fishing" = mean(.data$fishing_hours),
+                        "3rd Qu. fishing" = quantile(.data$fishing_hours, 0.75),
+                        "Max. fishing" = max(.data$fishing_hours),
+                        "Total hours" = sum(.data$hours),
+                        "Min. hours" = min(.data$hours),
+                        "1st Qu. hours" = quantile(.data$hours, 0.25),
+                        "Median hours" = median(.data$hours),
+                        "Mean hours" = mean(.data$hours),
+                        "3rd Qu. hours" = quantile(.data$hours, 0.75),
+                        "Max. hours" = max(.data$hours)
 )
 
   return(summarized)
